ba7cef2609535b3c34d18f03a8aa25a1e372404f
Lance Tipton <lancetipton04@gmail.com>
2021-05-22 06:11:48 -0700

    fix(utils): resolveFullPath * Added arg to resolve relative paths from custom location

--- repos/cli-utils/src/utils/resolveFullPath.js
+++ repos/cli-utils/src/utils/resolveFullPath.js
@@ -1,18 +1,18 @@
 const path = require('path')
 const homeDir = require('os').homedir()
-const curDir = process.cwd()
 
 /**
  * Resolves a relative path to a full path based on the passed in loc
  * If it starts with `./`, path is resolved from the process.cwd()
  * If it starts with `~/`, path is resolved from users home directory
  * @param {string} loc - Path to be resolved
+ * @param {string} [fromLoc=process.cwd()] - Relative paths are resolved from this location
  *
  * @returns {string} - Resolved full path 
  */
-const resolveFullPath = loc => {
+const resolveFullPath = (loc, fromLoc=process.cwd()) => {
   return loc.startsWith('./')
-    ? path.join(curDir, loc)
+    ? path.join(fromLoc, loc)
     : loc.startsWith('~/')
       ? path.join(homeDir, loc)
       : loc
