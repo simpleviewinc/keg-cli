22a402b30c78dbf08d92d6d59d9d7131c1b88840
lancetipton <lancetipton04@gmail.com>
2021-08-24 04:14:08 -0700

    fix(cli): Fix 5.2.2 update script

--- scripts/cli/updates/5.2.2.sh
+++ scripts/cli/updates/5.2.2.sh
@@ -5,16 +5,16 @@ source $(pwd)/keg
 
 source $(pwd)/scripts/cli/updates/update_helpers.sh
 
-keg_cli_5_2_1_update(){
+keg_cli_5_2_2_update(){
 
-  keg_message "Running update for version 5.2.1..."
+  keg_message "Running update for version 5.2.2..."
 
   keg cli env sync --conflict local --no-confirm
 
   echo ""
-  keg_message "5.2.1 Update Complete!"
+  keg_message "5.2.2 Update Complete!"
   echo ""
 
 }
 
-keg_cli_5_2_1_update "$@"
+keg_cli_5_2_2_update "$@"
